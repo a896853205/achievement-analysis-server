351984bad93d669e427255b8b469ad66f612af63
Qccccc's laptop <896853205@qq.com>
2019-10-01 16:18:08 +0800

    添加环境控制

--- src/resources/mysql-conf.js
+++ src/resources/mysql-conf.js
@@ -1,6 +1,7 @@
-import { DATA_BASE_KEY } from '../constants/keys';
-
-export const dbConfig = {
+import { DATA_BASE_KEY, LOCATION_DATA_BASE_KEY } from '../constants/keys';
+import { SAP_CONTROL } from '../config/app-config';
+import { ENVIRONMENT } from '../constants/app-constants';
+export let dbConfig = {
   host: '62.234.76.58',
   user: 'root',
   password: DATA_BASE_KEY,
@@ -9,3 +10,38 @@ export const dbConfig = {
   // 最大连接数，默认为10
   connectionLimit: 10
 };
+
+if (SAP_CONTROL === ENVIRONMENT.DEV) {
+  // 开发环境域名
+  dbConfig = {
+    host: 'localhost',
+    user: 'root',
+    password: LOCATION_DATA_BASE_KEY,
+    database: 'college',
+    port: 3306,
+    // 最大连接数，默认为10
+    connectionLimit: 10
+  };
+} else if (SAP_CONTROL === ENVIRONMENT.TEST) {
+  // 测试环境域名
+  dbConfig = {
+    host: 'localhost',
+    user: 'root',
+    password: LOCATION_DATA_BASE_KEY,
+    database: 'college',
+    port: 3306,
+    // 最大连接数，默认为10
+    connectionLimit: 10
+  };
+} else {
+  // 生产环境域名
+  dbConfig = {
+    host: '62.234.76.58',
+    user: 'root',
+    password: DATA_BASE_KEY,
+    database: 'college',
+    port: 3306,
+    // 最大连接数，默认为10
+    connectionLimit: 10
+  };;
+}
