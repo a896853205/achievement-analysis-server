483741cb673202c015a6b8a03b37c9ac99a13855
Qccccc's laptop <896853205@qq.com>
2019-10-03 12:37:42 +0800

    上线配置

--- src/config/app-config.js
+++ src/config/app-config.js
@@ -1,9 +1,9 @@
 import { ENVIRONMENT } from '../constants/app-constants';
 
 // 环境控制
-export const SAP_CONTROL = ENVIRONMENT.DEV;     // 开发环境
+// export const SAP_CONTROL = ENVIRONMENT.DEV;     // 开发环境
 // export const SAP_CONTROL = ENVIRONMENT.TEST; // 测试环境
-// export const SAP_CONTROL = ENVIRONMENT.PRO;  // 生产环境
+export const SAP_CONTROL = ENVIRONMENT.PRO;  // 生产环境
 
 // 项目版本号
 export const kAppVersion = 'v1.0.0';
@@ -14,4 +14,4 @@ export const kAppVersion = 'v1.0.0';
          99.99.99 版本为   999999000
          后三位为打包测试时使用 每打包一次+1
  */
-export const kAPPInfo_Appversion = '10000001';
+export const kAPPInfo_Appversion = '10000002';
