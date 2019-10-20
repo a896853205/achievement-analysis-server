03f518bcf7966a46e8401692827c2b06bab29f15
Qccccc's laptop <896853205@qq.com>
2019-10-20 14:01:21 +0800

    修改了主页文件的备注

--- src/service/index-data-service.js
+++ src/service/index-data-service.js
@@ -14,11 +14,10 @@ export default {
       newsDao.queryNewsProfileByType(4, 3)
     ]);
     /**
-  * 	
-1是指院校资讯，2是指专业资讯，3是考生必读，4是排名集锦
-按类型与指定数量返回，
-剩余三个未处理
-  */
+     * 1是指院校资讯，2是指专业资讯，3是考生必读，4是排名集锦
+     * 按类型与指定数量返回，
+     * 剩余三个未处理
+     */
     return {
       schoolNewsList,
       majorNewsList,
