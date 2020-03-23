3f6e429f635ecaf5acffef9f27650ed0e9b2c010
Qccccc's laptop <896853205@qq.com>
2020-03-23 17:42:48 +0800

    使用了一个sdk

--- src/service/payment-service.js
+++ src/service/payment-service.js
@@ -1,20 +1,9 @@
-import AlipaySdk from 'alipay-sdk';
+import alipaySdk from '../../util/alipay-sdk';
 import AlipayFormData from 'alipay-sdk/lib/form';
-import fs from 'fs';
 import uuid from 'uuid';
-import path from 'path';
 
 export default {
   getAlipayPaymentUrl: async user => {
-    const alipaySdk = new AlipaySdk({
-      // 参考下方 SDK 配置
-      appId: '2019121069819226',
-      privateKey: fs.readFileSync(
-        path.resolve(__dirname, '../../ssl/alipay-private-key.pem'),
-        'ascii'
-      )
-    });
-
     const formData = new AlipayFormData();
     // 调用 setMethod 并传入 get，会返回可以跳转到支付页面的 url
     formData.setMethod('get');
@@ -32,7 +21,7 @@ export default {
       body: `使用功能：学业测评、查数据、智能填报、志愿合理分析、就业前景分析等。
       适用对象：普通类文理科考生（自主招生、专科及艺术体育类考生暂不适用）。
       使用时效：有效期截止到用户自行选择高考年的9月1日为止。`,
-      passback_params: JSON.stringify({ userUuid: user.uuid })
+      passbackParams: JSON.stringify({ userUuid: user.uuid })
     });
 
     const result = await alipaySdk.exec(
@@ -46,15 +35,6 @@ export default {
   },
 
   signPayment: async postData => {
-    const alipaySdk = new AlipaySdk({
-      // 参考下方 SDK 配置
-      appId: '2019121069819226',
-      privateKey: fs.readFileSync(
-        path.resolve(__dirname, '../../ssl/alipay-private-key.pem'),
-        'ascii'
-      )
-    });
-
     console.log('postData', postData);
 
     let isSuccess = alipaySdk.checkNotifySign(postData);
---
+++ util/alipay-sdk.js
@@ -0,0 +1,12 @@
+import AlipaySdk from 'alipay-sdk';
+import fs from 'fs';
+import path from 'path';
+
+export default new AlipaySdk({
+  // 参考下方 SDK 配置
+  appId: '2019121069819226',
+  privateKey: fs.readFileSync(
+    path.resolve(__dirname, '../ssl/alipay-private-key.pem'),
+    'ascii'
+  )
+});
